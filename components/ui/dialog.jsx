6c9daa964064d84c200b69d491ede616064ef79f
Nikhil11072003 <2021.nikhil.thakur@ves.ac.in>
2024-07-16 18:20:00 +0530

    Fixed path issue in dialog.jsx

--- components/ui/dialog.jsx
+++ components/ui/dialog.jsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
 
-import { cn } from "/Users/nikhilthakur/Downloads/AI_Interviewer/ai-mock-interviewer/lib/utils.js"
+import { cn } from "/lib/utils"
 
 const Dialog = DialogPrimitive.Root
 
