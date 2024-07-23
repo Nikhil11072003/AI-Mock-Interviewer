fc7f8bfcc0c28837574e81b38c7eb2e0236a4eac
Nikhil11072003 <2021.nikhil.thakur@ves.ac.in>
2024-07-23 22:38:59 +0530

    Landing page and sign-up page issue solved

--- app/page.js
+++ app/page.js
@@ -8,18 +8,34 @@ import Link from "next/link";
 export default function Home() {
   const router=useRouter();
 
-  useEffect(() => {
+  /*useEffect(() => {
     router.push('/dashboard')
-  }, [])
+  }, [])*/
   return (
-    <div className="p-20 flex flex-col gap-10 items-center">
-    <div className="flex flex-col justify-center items-center gap-3">
-     <h2 className="text-3xl text-gray-600">Application started on 1 July 2024</h2>
-     <Link href='/dashboard'> <Button>Click here to get Started</Button></Link>
+    <section className="bg-gray-900 text-white">
+  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+    <div className="mx-auto max-w-3xl text-center">
+      <h1
+        className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+      >
+        AI Mock Interviewer
+
+      </h1>
+
+      <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+        Just select the role,job title and year of experience and have you virtual interview experience.
+      </p>
+
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+        <Link
+          className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+          href="/dashboard">Get Started</Link>
+      </div>
     </div>
-    <div>
-    <h2>This is AI mock interviewer web app made by Nikhil Thakur </h2>
-    </div>
-    </div>
+  </div>
+</section>
   );
 }
+
+
+
