5537ea5a43c5bee5fc8203b102d85eec2499fdda
Nikhil11072003 <2021.nikhil.thakur@ves.ac.in>
2024-07-23 22:17:21 +0530

    Fix routing issue on landing page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { Button } from "/components/ui/button"
+import Link from "next/link";
 
 export default function Home() {
   const router=useRouter();
@@ -14,7 +15,7 @@ export default function Home() {
     <div className="p-20 flex flex-col gap-10 items-center">
     <div className="flex flex-col justify-center items-center gap-3">
      <h2 className="text-3xl text-gray-600">Application started on 1 July 2024</h2>
-     <Button onClick={()=>{router.push('/dashboard')}}>Click here to get Started</Button>
+     <Link href='/dashboard'> <Button>Click here to get Started</Button></Link>
     </div>
     <div>
     <h2>This is AI mock interviewer web app made by Nikhil Thakur </h2>
